13de43b6b45c9f3590726a44a753423850a0596b
Yuxuan Ye <littleye233@gmail.com>
2026-08-06 00:43:23 +0800

    Remove gnome-icon-theme because it is unmaintained

--- home-modules/qt.nix
+++ home-modules/qt.nix
@@ -5,10 +5,10 @@ inputs:
 let
   cfg = config.programs.illogical-impulse;
   pythonEnv = cfg.internal.pythonEnv;
-  
+
   # The raw QuickShell package
   qsPackage = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
-  
+ 
 in
 {
   config = lib.mkIf cfg.enable {
@@ -18,17 +18,14 @@ in
       (pkgs.writeShellScriptBin "qs" ''
         # Comprehensive XDG_DATA_DIRS for icon and desktop file discovery
         export XDG_DATA_DIRS="${lib.makeSearchPath "share" [ 
-          pkgs.adwaita-icon-theme 
-          pkgs.hicolor-icon-theme 
+          pkgs.adwaita-icon-theme
+          pkgs.hicolor-icon-theme
           pkgs.papirus-icon-theme
-          pkgs.gnome-icon-theme
           pkgs.kdePackages.breeze-icons
           pkgs.lxqt.pavucontrol-qt
           pkgs.pavucontrol
         ]}:$HOME/.nix-profile/share:$HOME/.local/share:/etc/profiles/per-user/$USER/share:/run/current-system/sw/share:/usr/share:$XDG_DATA_DIRS"
-        
         export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-        
         # Launch the real binary
         exec ${qsPackage}/bin/qs "$@"
       '')
